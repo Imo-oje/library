5e11dfa3abe539e4f6ff4ae11cc1aec2267ec7e4
salvation <savvyplus020@gmail.com>
2023-07-30 17:16:27 +0100

    Enabled form pop up

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("console");
+const closeForm = document.querySelector(".form .close");
+const addBook = document.querySelector(".add-book");
+const form = document.querySelector(".form");
+
+let toggleForm = [closeForm, addBook];
+toggleForm.map((item) => {
+  item.addEventListener("click", () => {
+    form.classList.toggle("show");
+  });
+});
